9a3a91afe40ff94c07cdca4cd4da95c9cac51c69
Wang ShaoBo <bobo.shaobowang@huawei.com>
2021-06-25 16:05:13 +0800

    init client

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export WORKON_HOME=`pwd`
-export INSTALL_REPO_PATH=$WORKON_HOME/../repo
-export INSTALL_TARGET_BRANCH=openEuler-21.03
-export INSTALL_SOURCE_BRANCH=Questions
-export PYTHONPATH=$WORKON_HOME/gRPC:$WORKON_HOME/gRPC/proto
-
-python3 ./gRPC/dist_server.py
