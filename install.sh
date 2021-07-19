68a2b9948c5f031110b3c9e7bb7beeb295ca8258
wangshaobo <15122409391@163.com>
2021-07-19 10:27:42 +0800

    debug

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-echo WORKON_HOME=`pwd` >> ~/.bashrc
+echo "export WORKON_HOME=`pwd`" >> ~/.bashrc
 
 mkdir -p ~/oqserver
-echo INSTALL_REPO_PATH=~/oqserver/repo >> ~/.bashrc
-echo INSTALL_TARGET_BRANCH=openEuler-21.03 >> ~/.bashrc
-echo INSTALL_SOURCE_BRANCH=Questions >> ~/.bashrc
-echo PYTHONPATH=`pwd`/gRPC:`pwd`/gRPC/proto >> ~/.bashrc
+echo "export INSTALL_REPO_PATH=~/oqserver/repo" >> ~/.bashrc
+echo "export INSTALL_TARGET_BRANCH=openEuler-21.03" >> ~/.bashrc
+echo "export INSTALL_SOURCE_BRANCH=Questions" >> ~/.bashrc
+echo "export PYTHONPATH=`pwd`/gRPC:`pwd`/gRPC/proto" >> ~/.bashrc
 
 if [ -z "$COMMIT_FILE_PATH" ];then
 	COMMIT_DIR_PATH=~/oqserver/front
@@ -14,15 +14,16 @@ if [ -z "$COMMIT_FILE_PATH" ];then
 	COMMIT_FILE_PATH=$COMMIT_DIR_PATH/import.csv
 	touch $COMMIT_FILE_PATH
 fi
-echo IMPORT_COMMIT_FILE=$COMMIT_FILE_PATH >> ~/.bashrc
+echo "export IMPORT_COMMIT_FILE=$COMMIT_FILE_PATH" >> ~/.bashrc
 
 if [ -z "$REPORT_DIR_PATH" ];then
 	REPORT_DIR_PATH=~/oqserver/front
 	mkdir -p $REPORT_DIR_PATH
 fi
-echo OUTPUT_REPORT_DIR=$REPORT_DIR_PATH >> ~/.bashrc
+echo "export OUTPUT_REPORT_DIR=$REPORT_DIR_PATH" >> ~/.bashrc
 
-echo "alias oqserver=cd `pwd` && python3 ./gRPC/dist_server.py" >> ~/.bashrc
+echo "alias oqserver='cd `pwd` && python3 ./gRPC/dist_server.py'" >> ~/.bashrc
+source ~/.bashrc
 
 #export PATH=$PATH:/usr/local/bin/
 #python3 ./gRPC/dist_server.py > python.log 2>&1 &
--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-sed -i '' '/WORKON_HOME=/d' ~/.bashrc
+sed -i '/WORKON_HOME=/d' ~/.bashrc
 
-sed -i '' '/INSTALL_REPO_PATH=/d' ~/.bashrc
-sed -i '' '/INSTALL_TARGET_BRANCH=/d' ~/.bashrc
-sed -i '' '/INSTALL_SOURCE_BRANCH=/d' ~/.bashrc
-sed -i '' '/PYTHONPATH=/d' ~/.bashrc
-sed -i '' '/IMPORT_COMMIT_FILE=/d' ~/.bashrc
-sed -i '' '/alias oqserver=/d' ~/.bashrc
-sed -i '' '/OUTPUT_REPORT_DIR=/d' ~/.bashrc
+sed -i '/INSTALL_REPO_PATH=/d' ~/.bashrc
+sed -i '/INSTALL_TARGET_BRANCH=/d' ~/.bashrc
+sed -i '/INSTALL_SOURCE_BRANCH=/d' ~/.bashrc
+sed -i '/PYTHONPATH=/d' ~/.bashrc
+sed -i '/IMPORT_COMMIT_FILE=/d' ~/.bashrc
+sed -i '/alias oqserver=/d' ~/.bashrc
+sed -i '/OUTPUT_REPORT_DIR=/d' ~/.bashrc
+
+source ~/.bashrc
