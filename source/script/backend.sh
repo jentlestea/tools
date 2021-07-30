a8300d8d12df11e36059fba5cd34a9e05ab767ab
Wang ShaoBo <bobo.shaobowang@huawei.com>
2021-07-30 15:16:28 +0800

    debug

--- source/script/backend.sh
+++ source/script/backend.sh
@@ -12,7 +12,7 @@ if [[ ! -d $BK1 ]] || [[ ! -d $BK2 ]];then
 	echo -1
 fi
 
-tar -zxvf $time.bk1.tar.gz $BK1/*
-tar -zxvf $time.bk2.tar.gz $BK2/*
+tar -zcvf $BKPATH/$time/bk1.tar.gz $BK1/*
+tar -zcvf $BKPATH/$time/bk2.tar.gz $BK2/*
 
 echo 0
