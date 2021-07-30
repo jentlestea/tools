80a9efb135c00993fc119db3213c2744f75f50b6
Wang ShaoBo <bobo.shaobowang@huawei.com>
2021-07-30 15:29:34 +0800

    debug

--- source/install.sh
+++ source/install.sh
@@ -1,29 +1,36 @@
 #!/bin/sh
 
-echo "export WORKON_HOME=`pwd`" >> ~/.bashrc
+CONFPATH=/etc/profile.d/qs.sh
+
+if [ ! -f $CONFPATH ];then
+	exit
+fi
+
+if [ $# != 1 ];then
+	exit
+fi
+TARGETDIR=$1
+
+echo "export WORKON_HOME=`pw`" >> $CONFPATH
 
 mkdir -p ~/oqserver
-echo "export INSTALL_REPO_PATH=~/oqserver/repo" >> ~/.bashrc
-echo "export INSTALL_TARGET_BRANCH=openEuler-21.03" >> ~/.bashrc
-echo "export INSTALL_SOURCE_BRANCH=Questions" >> ~/.bashrc
-echo "export SPYTHONPATH=`pwd`/gRPC:`pwd`/gRPC/proto" >> ~/.bashrc
+echo "export INSTALL_REPO_PATH=~/oqserver/repo" >> $CONFPATH
+echo "export INSTALL_TARGET_BRANCH=openEuler-21.03" >> $CONFPATH
+echo "export INSTALL_SOURCE_BRANCH=Questions" >> $CONFPATH
+echo "export SPYTHONPATH=$TARGETDIR/gRPC:$TARGETDIR/gRPC/proto" >> $CONFPATH
 mkdir -p ~/oqserver/pool/ifs
-echo "export IFSPATH=~/oqserver/pool/ifs" >> ~/.bashrc
+echo "export IFSPATH=~/oqserver/pool/ifs" >> $CONFPATH
 if [ -z "$COMMIT_FILE_PATH" ];then
 	COMMIT_DIR_PATH=~/oqserver/front
 	mkdir -p $COMMIT_DIR_PATH
 	COMMIT_FILE_PATH=$COMMIT_DIR_PATH/import.csv
 	touch $COMMIT_FILE_PATH
 fi
-echo "export IMPORT_COMMIT_FILE=$COMMIT_FILE_PATH" >> ~/.bashrc
+echo "export IMPORT_COMMIT_FILE=$COMMIT_FILE_PATH" >> $CONFPATH
 if [ -z "$REPORT_DIR_PATH" ];then
 	REPORT_DIR_PATH=~/oqserver/front
 	mkdir -p $REPORT_DIR_PATH
 fi
-echo "export OUTPUT_REPORT_DIR=$REPORT_DIR_PATH" >> ~/.bashrc
-echo "alias oqserver='cd `pwd` && python3 ./gRPC/dist_server.py'" >> ~/.bashrc
-echo "alias oqserver-tool='python3 `pwd`/tools/oqserver-tool.py'" >> ~/.bashrc
-source ~/.bashrc
-
-#export PATH=$PATH:/usr/local/bin/
-#python3 ./gRPC/dist_server.py > python.log 2>&1 &
+echo "export OUTPUT_REPORT_DIR=$REPORT_DIR_PATH" >> $CONFPATH
+echo "alias oqserver='cd $TARGETDIR && python3 ./gRPC/dist_server.py'" >> $CONFPATH
+echo "alias oqserver-tool='python3 $TARGETDIR/tools/oqserver-tool.py'" >> $CONFPATH
--- source/uninstall.sh
+++ source/uninstall.sh
@@ -1,26 +1,3 @@
 #!/bin/sh
 
-if [[ `uname` == 'Darwin' ]];then
-	sed -i '' '/WORKON_HOME=/d' ~/.bashrc
-	sed -i '' '/INSTALL_REPO_PATH=/d' ~/.bashrc
-	sed -i '' '/INSTALL_TARGET_BRANCH=/d' ~/.bashrc
-	sed -i '' '/INSTALL_SOURCE_BRANCH=/d' ~/.bashrc
-	sed -i '' '/SPYTHONPATH=/d' ~/.bashrc
-	sed -i '' '/IMPORT_COMMIT_FILE=/d' ~/.bashrc
-	sed -i '' '/alias oqserver=/d' ~/.bashrc
-	sed -i '' '/OUTPUT_REPORT_DIR=/d' ~/.bashrc
-	sed -i '' '/IFSPATH=/d' ~/.bashrc
-	sed -i '' '/alias oqserver-tool=/d' ~/.bashrc
-else
-	sed -i '/WORKON_HOME=/d' ~/.bashrc
-	sed -i '/INSTALL_REPO_PATH=/d' ~/.bashrc
-	sed -i '/INSTALL_TARGET_BRANCH=/d' ~/.bashrc
-	sed -i '/INSTALL_SOURCE_BRANCH=/d' ~/.bashrc
-	sed -i '/SPYTHONPATH=/d' ~/.bashrc
-	sed -i '/IMPORT_COMMIT_FILE=/d' ~/.bashrc
-	sed -i '/alias oqserver=/d' ~/.bashrc
-	sed -i '/OUTPUT_REPORT_DIR=/d' ~/.bashrc
-	sed -i '/IFSPATH=/d' ~/.bashrc
-	sed -i '/alias oqserver-tool=/d' ~/.bashrc
-fi
-source ~/.bashrc
+rm -rf ~/oqserver
